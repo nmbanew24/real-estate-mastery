0c1a6a28ca24070894972887bcc6a7f871a5e26e
nmbanew24 <nmbanew24@gmail.com>
2024-11-23 18:33:14 -0600

    Make SMS service optional and more resilient

--- services/smsService.js
+++ services/smsService.js
@@ -1,11 +1,24 @@
 const twilio = require('twilio');
 
-const client = twilio(
-    process.env.TWILIO_ACCOUNT_SID,
-    process.env.TWILIO_AUTH_TOKEN
-);
+let client = null;
+
+// Only initialize Twilio if credentials are provided
+if (process.env.TWILIO_ACCOUNT_SID && 
+    process.env.TWILIO_AUTH_TOKEN && 
+    process.env.TWILIO_PHONE_NUMBER) {
+    client = twilio(
+        process.env.TWILIO_ACCOUNT_SID,
+        process.env.TWILIO_AUTH_TOKEN
+    );
+}
 
 const sendSMS = async (to, message) => {
+    // If Twilio is not configured, log message and return
+    if (!client) {
+        console.log('SMS would have been sent:', { to, message });
+        return true;
+    }
+
     try {
         const result = await client.messages.create({
             body: message,
@@ -16,7 +29,8 @@ const sendSMS = async (to, message) => {
         return true;
     } catch (error) {
         console.error('SMS sending failed:', error);
-        throw error;
+        // Don't throw error, just return false
+        return false;
     }
 };
 
@@ -32,7 +46,13 @@ const templates = {
 };
 
 module.exports = {
-    sendVerificationCode: (to, code) => sendSMS(to, templates.verificationCode(code)),
-    sendWelcomeMessage: (to, firstName) => sendSMS(to, templates.welcomeMessage(firstName)),
-    sendCourseEnrollmentMessage: (to, courseName) => sendSMS(to, templates.courseEnrollment(courseName))
+    sendVerificationCode: async (to, code) => {
+        return await sendSMS(to, templates.verificationCode(code));
+    },
+    sendWelcomeMessage: async (to, firstName) => {
+        return await sendSMS(to, templates.welcomeMessage(firstName));
+    },
+    sendCourseEnrollmentMessage: async (to, courseName) => {
+        return await sendSMS(to, templates.courseEnrollment(courseName));
+    }
 };
